3ae9d6635b1a838efc99567589ef09c4b22c4621
Armando_Rdh <Armando_Rdh@hotmail.com>
2023-04-07 02:24:53 -0700

    GA terminado

--- GA.cpp
+++ GA.cpp
@@ -5,10 +5,11 @@ void GA(int maxit, int popsize, float mutrate, float selection, int Nt) {
 
 	int keep = (selection * popsize);
 	float mincost = -9999999;
-	//vector<vector<int>> pop = Arreglo(popsize, Nt);
+	vector<vector<int>> pop = Arreglo(popsize, Nt);
 
-	vector<vector<int>> pop = { {0,1,2,3,4},{4,2,1,3,0},{0,3,4,1,2},{3,2,4,1,0},{4,1,0,3,2},{4,3,2,0,1},{2,4,0,1,3} };
-	vector<vector<float>> distancias = { {0, 10, 20, 30, 40}, {10, 0, 15, 25, 35}, {20, 15, 0, 14, 22}, {30, 25, 14, 0, 16}, {40, 35, 22, 16, 0} };
+	//vector<vector<int>> pop = { {0,1,3,2,4},{4,2,1,3,0},{0,3,4,1,2},{3,2,4,1,0},{4,1,0,3,2},{4,3,2,0,1},{2,4,0,1,3},{2,4,0,3,1} };
+	//vector<vector<float>> distancias = { {0, 10, 20, 30, 40}, {10, 0, 15, 25, 35}, {20, 15, 0, 14, 22}, {30, 25, 14, 0, 16}, {40, 35, 22, 16, 0} };
+	vector<vector<float>> distancias = { {0.00,180.00,50.10,21.60,104.00},{178.00,0.00,131.00,192.00,236.00}, {50.20, 135.00, 0.00, 64.50, 108.00},{23.10, 194.00, 63.50, 0.00, 85.30},{105.00, 239.00, 109.00, 86.10, 0.00} };
 	vector<float> costos = FCostos(pop, distancias);
 	Acomodar(pop, costos);
 
@@ -19,11 +20,11 @@ void GA(int maxit, int popsize, float mutrate, float selection, int Nt) {
 	meanC.push_back(CalcularPromedio(costos));
 
 	int iga = 0;
-	
+
 	while (iga < maxit) {
 		iga++;
 		int M = ceil((popsize - keep) / 2);
-		
+
 		double sum = 0;
 		int a = popsize / 2;
 		vector<double> prob;
@@ -31,14 +32,14 @@ void GA(int maxit, int popsize, float mutrate, float selection, int Nt) {
 		for (int i = 1; i < a + 1; i++) {
 			prob.push_back(i);
 			sum += prob[i - 1];
-			
+
 		}
-		
+
 
 		for (int i = 0; i < prob.size(); i++) {
 			prob[i] = prob[i] / sum;
 		}
-		
+
 		reverse(prob.begin(), prob.end());
 		vector<double> odds = { 0 };
 		double b = 0;
@@ -47,24 +48,18 @@ void GA(int maxit, int popsize, float mutrate, float selection, int Nt) {
 			b += value;
 			odds.push_back(b);
 		}
-		
+
 		vector<double> pick1(M);
 		vector<double> pick2(M);
-
-		pick1 = { 0.56365, 0.0016474 };
-		pick2 = { 0.963219, 0.965783 };
-
 		random_device rd;
 		mt19937 gen(rd());
 		uniform_real_distribution<double> distribution(0.0, 1.0);
-		/*for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++) {
 			pick1[i] = distribution(gen);
 			pick2[i] = distribution(gen);
-		}*/
-
-		for (int i = 0; i < pick1.size(); i++) {
-			cout << pick1[i] << endl << pick2[i] << "\n\n";
 		}
+
+		
 
 		int ic = 0;
 		vector<int> ma, pa;
@@ -82,15 +77,84 @@ void GA(int maxit, int popsize, float mutrate, float selection, int Nt) {
 			ic += 1;
 		}
 
-		for (int element : ma) {
-			cout << element<< "  ";
+		vector<int> ix;
+
+		for (int i = 0; i < keep; i += 2) {
+			ix.push_back(i);
 		}
-		cout << endl;
-		for (int element : pa) {
-			cout << element << "  ";
+
+		for (int i = 0; i < ma.size(); i++) {
+			vector<int> padre1 = pop[ma[i]];
+			vector<int> padre2 = pop[pa[i]];
+			vector<int> ciclos(padre1.size(), -1);
+			int numCiclo = 1;
+
+			auto cicloInicio = find(ciclos.begin(), ciclos.end(), -1);
+
+			// Cruce cíclico
+			while (cicloInicio != ciclos.end()) {
+				int pos = cicloInicio - ciclos.begin();
+
+				while (ciclos[pos] < 0) {
+					ciclos[pos] = numCiclo;
+
+					int valor = padre2[pos];
+					pos = find(padre1.begin(), padre1.end(), valor) - padre1.begin();
+				}
+
+				numCiclo += 1;
+				cicloInicio = find(ciclos.begin(), ciclos.end(), -1);
+			}
+			vector<int> hijo1(padre1.size());
+			vector<int> hijo2(padre1.size());
+			for (int i = 0; i < padre1.size(); ++i) {
+				if (ciclos[i] % 2 == 0) {
+					hijo1[i] = padre1[i];
+					hijo2[i] = padre2[i];
+				}
+				else {
+					hijo1[i] = padre2[i];
+					hijo2[i] = padre1[i];
+				}
+			}
+
+			pop[keep + ix[i]] = hijo1;
+			pop[keep + ix[i] + 1] = hijo2;
 		}
+
+		int nmut = ceil((popsize - 1) * Nt * mutrate);
+		vector<int> mrow;
+		vector<int> mcol;
+		vector<int> mcol2;
+
+		uniform_real_distribution<double> distributionMut1(1.0, popsize - 1);
+		uniform_real_distribution<double> distributionMut2(1.0, Nt - 1);
+
+		for (int i = 0; i < nmut; i++) {
+			mrow.push_back(ceil(distributionMut1(gen)));
+			mcol.push_back(ceil(distributionMut2(gen)));
+			mcol2.push_back(ceil(distributionMut2(gen)));
+		}
+
+
+		for (int i = 0; i < nmut; i++) {
+			
+			auto aux = pop[mrow[i]][mcol[i]];
+			auto aux2 = pop[mrow[i]][mcol2[i]];
+			pop[mrow[i]][mcol[i]] = aux2;
+			pop[mrow[i]][mcol2[i]] = aux;
+
+		}
+
+		costos = FCostos(pop, distancias);
+		Acomodar(pop, costos);
 	}
 
-	
-	
+	for (int i = 0; i < pop.size(); i++) {
+		for (int j = 0; j < pop[i].size(); j++) {
+			cout << pop[i][j];
+		}
+		cout << endl << costos[i] << endl << endl;;
+	}
 }
+	
--- Source.cpp
+++ Source.cpp
@@ -7,39 +7,7 @@ int main() {
 	int popsize = 7;
 	int Nt = 4;
 
-	GA(1, 7, .1, .5, 4);
-	
-	/*// poblacion de enteros, sin repetir
-	vector<vector<int>> pop = Arreglo(popsize, Nt);
-
-	// Matriz provisional para pruebas
-	vector<vector<float>> distancias = { {0, 10, 20, 30, 40}, {10, 0, 15, 25, 35}, {20, 15, 0, 14, 22}, {30, 25, 14, 0, 16}, {40, 35, 22, 16, 0} };
-
-	// Calculo de costos
-	vector<float> costos = FCostos(pop, distancias);
-
-	//Impresion antes de acomodar por costos
-	for (int i = 0; i < pop.size() ; i++) {
-
-		for (int j = 0; j < pop[i].size() ; j++) {
-			cout << pop[i][j] << " ";
-		}
-		cout << endl;
-		cout << "Costo arreglo " << i + 1 << ": " << costos[i] << "\n\n";
-	}
-
-	Acomodar(pop, costos);
-
-	cout << "------------------------------\n";
-	//Impresion despues de acomodar por costos
-	for (int i = 0; i < pop.size(); i++) {
-
-		for (int j = 0; j < pop[i].size(); j++) {
-			cout << pop[i][j] << " ";
-		}
-		cout << endl;
-		cout << "Costo arreglo " << i + 1 << ": " << costos[i] << "\n\n";
-	}*/
+	GA(1000, 200, .5, .5, 4);
 
 	return 0;
 }
